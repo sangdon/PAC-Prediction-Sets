f653274e35830ab54216cf11c9049ab04dac87c6
Sangdon Park <ggdons@gmail.com>
2021-08-30 17:42:40 -0400

    PredSetBox added

--- model/__init__.py
+++ model/__init__.py
@@ -2,5 +2,6 @@ from .util import *
 from .resnet import ResNet18, ResNet152
 from .temp import Temp, TempReg
 from .pred_set import PredSetCls, PredSetReg
+from .pred_set_det import PredSetBox
 
 from .fnn import Linear, SmallFNN, MidFNN, BigFNN
